cfeb779e66308762ea97a767e9e200012625a21c
FernandoGzm <fernandogzmr@gmail.com>
2013-11-09 21:07:01 +0000

    Language selector & Spanish translation
    A HTML selector is shown to choose the text language
    Spanish translation added

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 xkcd-substitutions
 ==================
 
-Small script that make reading news more fun - based on http://xkcd.com/1288/
+Fork from a "Small script that make reading news more fun - based on http://xkcd.com/1288/" by [Michał Kowalkowski](https://github.com/michalkow)
+
+Spanish translation added with language selector
 
 Usage
 --------
 
 Just add the code below to your bookmarks.
 
-    javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='https://raw.github.com/michalkow/xkcd-substitutions/master/substitutions.js';void(0);
-    
-				
+    javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='https://raw.github.com/fernandogzm/xkcd-substitutions/master/substitutions.js';void(0);
+
+Select the correct language to translate the words
+
 License
 -------
 xkcd-substitutions script is released under [MIT license](http://opensource.org/licenses/mit-license.php).
 
 Credits
 -------
-xkcd-substitutions script was created by [Michał Kowalkowski](https://github.com/michalkow). You can contact me at [kowalkowski.michal@gmail.com](mailto:kowalkowski.michal@gmail.com)
+Original xkcd-substitutions script was created by [Michał Kowalkowski](https://github.com/michalkow).
+
+This versions was modified by [FernandoGzm](https://github.com/FernandoGzm).
--- substitutions.js
+++ substitutions.js
@@ -1,27 +1,69 @@
+function chooseLanguage(){
+	var langs = ["--Choose--" , "English", "Spanish"];
+	var s = document.createElement('select');
+
+
+	for (var i = 0; i < langs.length; i++) {
+		var opt;
+		opt = document.createElement('option');
+		opt.value = langs[i];
+		opt.innerHTML = langs[i] ;
+		s.appendChild(opt);
+	}
+	s.id="xkcdSubs";
+	s.setAttribute("onChange", "xkcdSubstitutions()");
+	s.style.position ="fixed";
+	s.style.top = "10px";
+	s.style.cssFloat="left";
+	s.style.zIndex="99999";
+	document.body.appendChild(s);
+
+}
+
 function xkcdSubstitutions(){
-	var words = [
-		{find: /witnesses/gi, replace: 'these dudes I know'},
-		{find: /allegedly/gi, replace: 'kinda probably'},
-		{find: /new study/gi, replace: 'tumbler post'},
-		{find: /rebuild/gi, replace: 'avenge'},
-		{find: /space/gi, replace: 'spaaace'},
-		{find: /google glass/gi, replace: 'virtual boy'},
-		{find: /smartphone/gi, replace: 'Pokédex'},
-		{find: /electric/gi, replace: 'atomic'},
-		{find: /senator/gi, replace: 'elf-lord'},
-		{find: /car/gi, replace: 'cat'},
-		{find: /election/gi, replace: 'eating contest'},
-		{find: /congressional leaders/gi, replace: 'river spirits'},
-		{find: /homeland security/gi, replace: 'homestar runner'},
-		{find: /could not be reached for comment/gi, replace: 'is guilty and everyone knows it'},
-	];
+	var lang = document.getElementById("xkcdSubs").value.toLowerCase();
+	var words = {
+		english:[
+			{find: /witnesses/gi, replace: 'these dudes I know'},
+			{find: /allegedly/gi, replace: 'kinda probably'},
+			{find: /new study/gi, replace: 'tumbler post'},
+			{find: /rebuild/gi, replace: 'avenge'},
+			{find: /space/gi, replace: 'spaaace'},
+			{find: /google glass/gi, replace: 'virtual boy'},
+			{find: /smartphone/gi, replace: 'Pokédex'},
+			{find: /electric/gi, replace: 'atomic'},
+			{find: /senator/gi, replace: 'elf-lord'},
+			{find: /car/gi, replace: 'cat'},
+			{find: /election/gi, replace: 'eating contest'},
+			{find: /congressional leaders/gi, replace: 'river spirits'},
+			{find: /homeland security/gi, replace: 'homestar runner'},
+			{find: /could not be reached for comment/gi, replace: 'is guilty and everyone knows it'},
+		],
+
+		spanish: [
+			{find: /testigos/gi, replace: 'tíos esos'},
+			{find: /intencionadamente/gi, replace: '"sin querer"'},
+			{find: /nuevo estudio/gi, replace: 'post de tumblr'},
+			{find: /recuperar/gi, replace: 'vengar'},
+			{find: /espacio/gi, replace: 'espaaaaacio'},
+			{find: /google glass/gi, replace: 'virtual boy'},
+			{find: /smartphone/gi, replace: 'movilaco'},
+			{find: /eléctrica/gi, replace: 'nucelar'},
+			{find: /político/gi, replace: 'elf-lord'},
+			{find: /coche/gi, replace: 'gatico'},
+			{find: /elecciones/gi, replace: 'concurso de comida'},
+			{find: /presidente/gi, replace: 'espíritus ancestrales'},
+			{find: /seguridad/gi, replace: 'con securitas'},
+			{find: /no está disponible/gi, replace: 'es culpable y todos lo sabemos'}
+		]
+	};
 
 	var html = document.body.innerHTML;
-	for (var i = 0; i < words.length; i++) {
-		html = html.replace(words[i].find, words[i].replace);
-	}; 
-	
+	for (var i = 0; i < words[lang].length; i++) {
+			html = html.replace(words[lang][i].find, words[lang][i].replace);
+	};
+	document.body.innerHTML= html;
 	return html;
 }
 
-document.body.innerHTML=xkcdSubstitutions();
+var option = chooseLanguage();
